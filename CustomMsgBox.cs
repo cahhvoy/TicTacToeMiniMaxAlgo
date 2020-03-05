28d817594309a7c923700b0ba671f4a8ba738e5d
cahhvoy <cahhvoy@gmail.com>
2020-03-05 21:14:31 +0300

    working basic version {2 Player}

---
+++ CustomMsgBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_tac_Toe
+{
+    public partial class CustomMsgBox : Form
+    {
+        public CustomMsgBox()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomMsgBox_Load(object sender, EventArgs e)
+            {
+              //converting a label into a divider line 
+                labelTopLine.AutoSize = false;
+                labelTopLine.Text="";
+                labelTopLine.Height = 1;
+                labelTopLine.Width = 370;
+                labelTopLine.BorderStyle = BorderStyle.None;
+                labelTopLine.BackColor = Color.Orange;
+
+            labelBotLine.AutoSize = false;
+            labelBotLine.Text = "";
+            labelBotLine.Height = 1;
+            labelBotLine.Width = 300;
+            labelBotLine.BorderStyle = BorderStyle.None;
+            labelBotLine.BackColor = Color.Orange;
+        }
+
+       
+
+        private void buttonClose_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+    }
+}
--- TicTacToeMain.cs
+++ TicTacToeMain.cs
@@ -21,14 +21,28 @@ namespace Tic_tac_Toe
         public int turns = 0; //Counting Turns
         
         //Counting wins for both Players and Draws
-        public int playerX = 0;
-        public int playerY = 0;
-        public int playerDraw = 0;
+        public int playerXWins = 0;
+        public int playerOWins = 0;
+        public int playersDraw = 0;
         
 
         private void TictacToe_Load(object sender, EventArgs e)
         {
             // this is method a form calls during  loading
+            //draw top line
+                labelTopLine.AutoSize = false;
+                labelTopLine.Text = "";
+                labelTopLine.Height = 1;
+                labelTopLine.Width = 380;
+                labelTopLine.BorderStyle = BorderStyle.None;
+                labelTopLine.BackColor = Color.DarkOliveGreen;
+
+            //draw bottom line 
+                label1.AutoSize = false;
+                label1.Height = 1;
+                label1.Width = 380;
+                label1.BackColor = Color.Maroon;
+                label1.BorderStyle = BorderStyle.None;
 
         }
 
@@ -39,17 +53,46 @@ namespace Tic_tac_Toe
             if (button.Text == "")
             {
                 if (currPlayer % 2 == 0)
-                {
-                    button.Text = "X";
-                    currPlayer++;
-                    turns++;
-                }
+                    {
+                        button.Text = "X";
+                        currPlayer++;
+                        turns++;
+                    }
                 else
-                {
-                    button.Text = "O";
-                    currPlayer++;
-                    turns++;
-                }
+                    {
+                        button.Text = "O";
+                        currPlayer++;
+                        turns++;
+                    }
+                if (CheckForAWin() == true)
+                    {
+                        if (button.Text == "X")
+                            {
+                                //CustomMsgBox msgBox = new CustomMsgBox();
+                                //msgBox.ShowDialog();
+                                //MessageBox.Show("Player X Wins");
+                                playerXWins++;                        
+                                labelXWinsVal.Text = playerXWins.ToString();
+                                NewGame();
+                            }
+                        else
+                            {
+                                MessageBox.Show("Player O Wins");
+                                playerOWins++;
+                                labelOwinsVal.Text = playerOWins.ToString();
+                                NewGame();                    
+                            }
+                    }
+                if (CheckDraw() == true)
+                    {
+                        MessageBox.Show("Its A Draw");
+                        playersDraw++;
+                        labelDrawsVal.Text = playersDraw.ToString();                   
+                        NewGame();
+                   }
+                //check if draw or win  every time a move is Made
+                //if()
+            
             }
 
         }
@@ -70,12 +113,12 @@ namespace Tic_tac_Toe
             currPlayer = 2;
             turns = 0;
             foreach( Control button in groupBoxGameBtns.Controls)
-            {
-                if (button.GetType() == typeof(Button))
                 {
-                    button.Text = "";
+                    if (button.GetType() == typeof(Button))
+                        {
+                            button.Text = "";
+                        }
                 }
-            }
 
         }
 
@@ -84,18 +127,79 @@ namespace Tic_tac_Toe
             NewGame();
         }
 
-        bool checkDraw()
+        bool CheckDraw()
         {
-            if (turns == 9)
-            {
-                return true;
-            }
+            if (turns == 9 && CheckForAWin()==false)
+                {
+                    return true;
+                }
             else
-            {
-                return false;
-            }
+                {
+                    return false;
+                }
         }
 
-
+        bool CheckForAWin()
+            {
+              ///horizontal wins
+                  if((A01.Text==A02.Text) && (A02.Text == A03.Text) && A01.Text !="")
+                        {
+                          return true;
+                        }
+                  else if((A11.Text==A12.Text) && (A12.Text == A13.Text) && A11.Text !="")
+                        {
+                          return true;
+                        }
+                  else if ((A21.Text == A22.Text) && (A22.Text == A23.Text) && A21.Text != "")
+                        {
+                            return true;
+                        }
+              ///vertical wins
+                  else if((A01.Text == A11.Text) && (A11.Text == A21.Text) && A01.Text != "")
+                        {
+                            return true;
+                        }
+                  else if ((A02.Text == A12.Text) && (A12.Text == A22.Text) && A02.Text != "")
+                        {
+                            return true;
+                        }
+                  else if((A03.Text == A13.Text) && (A13.Text == A23.Text) && A03.Text != "")
+                        {
+                            return true;
+                        }
+                   ///Diagonal wins
+                  else if ((A01.Text == A12.Text) && (A12.Text == A23.Text) && A01.Text != "")
+                        {
+                            return true;
+                        }
+                  else if ((A03.Text == A12.Text) && (A12.Text == A21.Text) && A03.Text != "")
+                        {
+                            return true;
+                        }
+                  //else if ((A03.Text == A13.Text) && (A13.Text == A23.Text) && A03.Text != "")
+                  //      {
+                  //          return true;
+                  //      }
+                    else
+                        {
+                            return false;
+                        }
+                }
+        private void buttonResetGame_Click(object sender, EventArgs e)
+            {
+                NewGame();
+                
+                labelXWinsVal.Text = "0";
+                labelOwinsVal.Text = "0";
+                labelDrawsVal.Text = "0";
+                playerXWins = 0;
+                playerOWins = 0;
+                playersDraw = 0;
+            }
+    
+    
+    
+    
+    
     }
 }
